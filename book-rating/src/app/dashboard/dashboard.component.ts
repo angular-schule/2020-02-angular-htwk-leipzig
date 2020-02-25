e2e48deeecef11375931ad07141365cf127b1539
Ferdinand Malcher <ferdinand@malcher.media>
2020-02-25 12:10:41 +0100

    Dashboard mit string[]

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: string[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.books = [
+      'Angular', 'React'
+    ];
+  }
+
+}
