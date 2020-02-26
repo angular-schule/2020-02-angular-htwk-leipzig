bec2b92db7ca029dbb74a40e7b48b6983b3842fd
Ferdinand Malcher <ferdinand@malcher.media>
2020-02-26 13:55:37 +0100

    Tests: BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
 
 describe('BookRatingService', () => {
   let service: BookRatingService;
+  let book: Book;
 
   beforeEach(() => {
+    // Arrange
     TestBed.configureTestingModule({});
     service = TestBed.inject(BookRatingService);
+
+    book = {
+      isbn: '',
+      title: '',
+      rating: 3,
+      description: ''
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should rate up a book', () => {
+    // Act
+    const ratedBook = service.rateUp(book);
+
+    // Assert
+    expect(ratedBook.rating).toBe(4);
+  });
+
+  it('should not rate higher than 5', () => {
+    // Arrange
+    book.rating = 5;
+
+    // Act
+    const ratedBook = service.rateUp(book);
+
+    // Assert
+    expect(ratedBook.rating).toBe(5);
   });
 });
